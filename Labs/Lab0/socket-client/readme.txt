ba2ff6274e49f6c8d5b72eb4c0cf909af8af26bb
Ivan Santos <vanil90@gmail.com>
2016-02-23 16:31:15 +0000

    Adicionado solução para o Lab0
    Implementado o lado do cliente que espera pela receção de uma mensagem
    de confirmação do servidor.

---
+++ Labs/Lab0/socket-client/readme.txt
@@ -0,0 +1,47 @@
+This example demonstrates the use of sockets in the Java platform.
+The client connects to the server, sends a line of text, and terminates.
+
+
+Instructions using Maven:
+------------------------
+
+To compile:
+  mvn compile
+
+To run using exec plugin:
+    mvn exec:java
+
+To generate launch scripts for Windows and Linux:
+  (appassembler:assemble is attached to install phase)
+  mvn install
+
+To run using appassembler plugin:
+  On Windows:
+    target\appassembler\bin\socket-client localhost 8000 test 1 2 3
+  On Linux:
+    ./target/appassembler/bin/socket-client localhost 8000 test 1 2 3
+
+
+To configure the Maven project in Eclipse:
+-----------------------------------------
+
+If Maven pom.xml exist:
+    'File', 'Import...', 'Maven'-'Existing Maven Projects'
+    'Select root directory' and 'Browse' to the project base folder.
+	Check that the desired POM is selected and 'Finish'.
+
+If Maven pom.xml do not exist:
+    'File', 'New...', 'Project...', 'Maven Projects'.
+	Check 'Create a simple project (skip architype selection)'.
+	Uncheck  'Use default Workspace location' and 'Browse' to the project base folder.
+	Fill the fields in 'New Maven Project'.
+	the Check if everything is OK and 'Finish'.
+
+To run:
+    Select the main class and click 'Run' (the green play button).
+    Specify arguments using 'Run Configurations'
+
+
+--
+Revision date: 2016-02-16
+leic-sod@disciplinas.tecnico.ulisboa.pt
---
+++ Labs/Lab0/socket-client/src/main/java/example/SocketClient.java
@@ -0,0 +1,55 @@
+package example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class SocketClient {
+
+	public static void main(String[] args) throws IOException {
+		// Check arguments
+		if (args.length < 3) {
+			System.err.println("Argument(s) missing!");
+			System.err.printf("Usage: java %s host port file%n", SocketClient.class.getName());
+			return;
+		}
+
+		String host = args[0];
+		// Convert port from String to int
+		int port = Integer.parseInt(args[1]);
+		// Concatenate arguments using a string builder
+		StringBuilder sb = new StringBuilder();
+		for (int i = 2; i < args.length; i++) {
+			sb.append(args[i]);
+			if (i < args.length - 1) {
+				sb.append(" ");
+			}
+		}
+		String text = sb.toString();
+
+		// Create client socket
+		Socket socket = new Socket(host, port);
+		System.out.printf("Connected to server %s on port %d %n", host, port);
+
+		// Create stream to receive data from server
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+		// Create stream to send data to server
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+		// Send text to server as bytes
+		out.writeBytes(text);
+		out.writeBytes("\n");
+		System.out.println("Sent text: " + text);
+
+		// Read response from server
+		String response = in.readLine();
+		System.out.println("Received from server: " + response);
+
+		// Close client socket
+		socket.close();
+		System.out.println("Connection closed");
+	}
+}

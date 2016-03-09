35ec834f1a39dee0980dd21178c2c95e55d3a186
Ivan Santos <vanil90@gmail.com>
2016-03-09 10:28:47 +0000

    Removed Tiago implementation of Lab3

--- Labs/Lab3/ttt-rmi-server/src/main/java/ttt/TTTService.java
+++
@@ -1,9 +0,0 @@
-package ttt;
-import java.rmi.*;
-
-
-public interface TTTService extends Remote {
-    public String currentBoard() throws RemoteException;
-    public boolean play(int row, int column, int player) throws RemoteException;
-    public int checkWinner() throws RemoteException;
-}
